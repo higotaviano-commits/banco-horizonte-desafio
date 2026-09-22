e958a32c805937f8b4188d96d9f25b505440a1fe
Higo Otaviano da Rocha <higo.rocha@caixa.gov.br>
2026-09-22 13:47:06 -0300

    primeiro commit

---
+++ src/test/java/bancohorizonte/org/AgenciaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package bancohorizonte.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaBancariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
